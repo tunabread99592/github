8ce69f4927e69e238c4284d19b4c6bfb09b391d0
tunabread99592 <thleong@ucdavis.edu>
2026-03-09 22:13:16 -0700

    decent progress

--- game/codeV1/script.js
+++ game/codeV1/script.js
@@ -4,13 +4,19 @@
 
     const startGame = document.querySelector('#startgame');
     const gameControl = document.querySelector('#gamecontrol');
-    const game = document.querySelector('#score');
+    const game = document.querySelector('#game');
+    const scoreBoard = document.querySelector('#score');
     const actionArea = document.querySelector('#actions');
 
+    const healthBar = document.querySelectorAll('.health-bar');
+
     const gameData = {
-        dice: ['1die.jpg', '2die.jpg', '3die.jpg',
-            '4die.jpg', '5die.jpg', '6die.jpg'],
+        dice: ['1die.png', '2die.png', '3die.png',
+            '4die.png', '5die.png', '6die.png'],
         players: ['player 1', 'player 2'],
+
+        characters: ['wizard1.png', 'pirate1.png', 'ninja1.png', 'cowboy1.png'],
+
         score: [0, 0],
         roll1: 0,
         roll2: 0,
@@ -19,12 +25,61 @@
         gameEnd: 100,
     };
 
-    let attacker;
-    let defender;
-    let defenderIndex;
+//player customization ----------------------------
 
+    const selectors = document.querySelectorAll('.character-select');
+    
+    let characterIndex = [];
+
+    selectors.forEach(function(selector,player){
+        const img = selector.querySelector('.displayed');
+        const prev = selector.querySelector('.left');
+        const next = selector.querySelector('.right');
+        
+        characterIndex[player] = 0;
+
+        function updateImage() {
+            img.src = `images/${gameData.characters[characterIndex[player]]}`;
+        }
+
+        updateImage();
+
+        //NEXT 
+        next.addEventListener('click', function(){
+            characterIndex[player] = (characterIndex[player] + 1) % gameData.characters.length;
+
+            updateImage();
+        });
+
+        //PREVIOUS
+        prev.addEventListener('click', function(){
+            characterIndex[player] = (characterIndex[player] - 1 + gameData.characters.length) % gameData.characters.length;
+            updateImage();
+        });
+    });
+
+   
+// start game --------------------------------------
 
     startGame.addEventListener('click', function(){
+
+        const arrows = document.querySelectorAll('.pChoose');
+        arrows.forEach(function(arrow){
+            arrow.classList.replace('showing','hidden');
+        })
+
+        healthBar.forEach(function(bar){
+            bar.classList.replace('hidden','showing');
+        })
+
+        gameData.selectedCharacters = [
+            gameData.characters[characterIndex[0]],
+            gameData.characters[characterIndex[1]]
+        ];
+
+        updateBar(0);
+        updateBar(1);
+
         gameControl.innerHTML = '<h2>The Game Has Started</h2>';
         gameControl.innerHTML += '<button id="quit">Wanna Quit?</button>';
 
@@ -38,6 +93,19 @@
         console.log('set up the turn');
     });
 
+//base functions------------------------------------
+
+    function proceed() {
+        actionArea.innerHTML = '<button id="rollagain">Roll again</button> or <button id="pass">Pass</button>';
+        document.querySelector('#rollagain').addEventListener('click', function(){
+            throwDice();
+        });
+        document.querySelector('#pass').addEventListener('click', function(){
+            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
+            setUpTurn();
+        });
+    }
+
     function setUpTurn() {
         game.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
         actionArea.innerHTML = '<button id="roll">Roll the Dice</button>';
@@ -48,173 +116,9 @@
         });
     }
 
-    function throwDice(){
-        actionArea.innerHTML = '';
-        gameData.roll1 = Math.floor(Math.random()*6) + 1;
-        gameData.roll2 = Math.floor(Math.random()*6) + 1;
-
-        game.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
-        game.innerHTML += `<img src="images/${gameData.dice[gameData.roll1-1]}">
-        <img src="images/${gameData.dice[gameData.roll2-1]}">`;
-
-        gameData.rollSum = gameData.roll1 + gameData.roll2;
-
-
-        if(gameData.index){
-            attacker = gameData.score[0];
-            defender = gameData.score[1];
-            // defenderIndex = 0;
-        }
-        else {
-            attacker = gameData.score[1];
-            defender = gameData.score[0];
-            // defenderIndex = 1;
-        }
-
-        // if two 1s are rolled
-        if(gameData.rollSum === 2) {
-            console.log("snake eyes");
-            game.innerHTML+= '<p>Oh snap! Snake eyes!</p>';
-            //set the score for the current player
-            gameData.score = (gameData.index = 0);
-            gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-            //we will ad showCurrentScore() function here...
-            //wait 2 seconds
-            setTimeout(setUpTurn, 2000);
-            showCurrentScore();
-        }
-
-
-        else if(gameData.roll1 === 6) {
-            console.log('Dice 1 is a 6');
-            game.innerHTML+= '<p>A direct hit!<p>';
-
-            gameData.score[defender] = defender - gameData.roll2;
-            console.log(gameData.score[gameData.index]);
-            console.log(gameData.roll2);
-
-
-
-
-
-
-
-
-
-            actionArea.innerHTML = '<button id="rollagain">Roll again</button> or <button id="pass">Pass</button>';
-
-            document.querySelector('#rollagain').addEventListener('click', function(){
-                throwDice();
-            });
-
-            document.querySelector('#pass').addEventListener('click', function(){
-                gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-                setUpTurn();
-            });
-
-            checkWinningCondition();
-
-            // bottomLine();
-        }
-
-
-        else if (gameData.roll2 === 6) {
-            console.log('Dice 2 is a 6');
-            game.innerHTML+= '<p>A direct hit!<p>';
-
-            gameData.score[defender] = defender - gameData.roll1;
-            console.log(gameData.score[gameData.index]);
-            console.log(gameData.roll1);
-
-
-
-
-
-
-
-            actionArea.innerHTML = '<button id="rollagain">Roll again</button> or <button id="pass">Pass</button>';
-
-            document.querySelector('#rollagain').addEventListener('click', function(){
-                throwDice();
-            });
-
-            document.querySelector('#pass').addEventListener('click', function(){
-                gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-                setUpTurn();
-            });
-
-            checkWinningCondition();
-
-            // bottomLine();
-        }
-
-        else {
-            console.log('neither die was a 1, so the game continues');
-            gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
-            actionArea.innerHTML = '<button id="rollagain">Roll again</button> or <button id="pass">Pass</button>';
-
-            document.querySelector('#rollagain').addEventListener('click', function(){
-                throwDice();
-            });
-
-            document.querySelector('#pass').addEventListener('click', function(){
-                gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-                setUpTurn();
-            });
-
-            checkWinningCondition();
-        }
-
-        console.log
-            (`player 1 score: ${gameData.score[0]} 
-            player 2 score: ${gameData.score[1]}
-            ${gameData.roll1}, ${gameData.roll2}`
-            );
-     
-        // gameData.roll1 = 1;
-        // gameData.roll2 = 1;
-
-
-        
-        // else if(gameData.roll1 === 1 || gameData.roll2 === 1) {
-        //     console.log('one of the two dice rolled is a 1');
-        //     gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-        //     game.innerHTML += `<p>Sorry, one of your rolls was a one, switching to ${gameData.players[gameData.index]}</p>`;
-        //     setTimeout(setUpTurn, 2000);
-        // }
-
-        
-        // else {
-        //     console.log('neither die was a 1, so the game continues');
-        //     gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
-        //     actionArea.innerHTML = '<button id="rollagain">Roll again</button> or <button id="pass">Pass</button>';
-
-        //     document.querySelector('#rollagain').addEventListener('click', function(){
-        //         throwDice();
-        //     });
-
-        //     document.querySelector('#pass').addEventListener('click', function(){
-        //         gameData.index ? (gameData.index = 0) : (gameData.index = 1);
-        //         setUpTurn();
-        //     });
-
-        //     checkWinningCondition();
-        // }
-    }
-
-    function bottomLine(){
-        if(gameData.score[0]<0){
-            gameData.score[0]=0;
-        }
-        else if(gameData.score[1]<0){
-            gameData.score[1]=0;
-        };
-    }
-
     function checkWinningCondition() {
-        // console.log(gameData.players[gameData.index], gameData.score[gameData.index]);
-        if (gameData.score[gameData.index] > gameData.gameEnd) {
-            score.innerHTML = `<h2>${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points!</h2>`;
+        if (gameData.score[gameData.index] >= gameData.gameEnd) {
+            game.innerHTML = `<h2>${gameData.players[gameData.index]} wins with ${gameData.score[gameData.index]} points!</h2>`;
 
             actionArea.innerHTML = '';
             document.querySelector('#quit').innerHTML = 'Start a New Game?';
@@ -224,7 +128,128 @@
     }
 
     function showCurrentScore() {
-        score.innerHTML = `<p>The score is currently <strong>${gameData.players[0]} : ${gameData.score[0]}</strong> and <strong>${gameData.players[1]} : ${gameData.score[1]}</strong></p>`;
+        scoreBoard.innerHTML =
+         `<p>The score is currently
+         <strong>${gameData.players[0]} : ${gameData.score[0]}</strong> and
+         <strong>${gameData.players[1]} : ${gameData.score[1]}</strong></p>`;
+    }
+
+    function switchPlayer(){
+        gameData.index = 1 - gameData.index;
+    }
+
+    function rollDice(){
+        // game.innerHTML = `<p>Rolling...</p>`
+        // game.innerHTML += `
+        //     <img class="dice" src="images/diceRoll.gif">
+        //     <img class="dice" src="images/diceRoll.gif">`;
+
+        // setTimeout(function(){
+        //     gameData.roll1 = Math.floor(Math.random()*6) + 1;
+        //     gameData.roll2 = Math.floor(Math.random()*6) + 1;
+        //     gameData.rollSum = gameData.roll1 + gameData.roll2;
+
+        //     displayDice();
+        // },2000);
+
+        gameData.roll1 = Math.floor(Math.random()*6) + 1;
+        gameData.roll2 = Math.floor(Math.random()*6) + 1;
+        gameData.rollSum = gameData.roll1 + gameData.roll2;
+    };
+
+    function displayDice(){
+        game.innerHTML = `<p>Roll the dice for the ${gameData.players[gameData.index]}</p>`;
+        
+        game.innerHTML += `
+        <img class="dice" src="images/${gameData.dice[gameData.roll1-1]}">
+        <img class="dice" src="images/${gameData.dice[gameData.roll2-1]}">`;
+    }
+
+    function celebrate(player){
+        const img = document.querySelectorAll('.displayed')[player];
+
+        const normal = img.src;
+        const celebrate = normal.replace('1.png','2.png');
+        img.src = celebrate;
+
+        setTimeout(function(){
+            img.src = normal;
+        },2000);
+    }
+
+    function updateBar(playerIndex) {
+        const bar = healthBar[playerIndex].querySelector('.bar');
+        const points = gameData.score[playerIndex];
+        const total = parseInt(healthBar[playerIndex].dataset.total);
+        const percent = Math.min(100, (points/total)*100);
+
+        bar.style.width = percent + '%';
+
+        healthBar[playerIndex].dataset.value = points;
+    }
+
+// playing--------------------------------
+
+    function throwDice(){
+        actionArea.innerHTML = '';
+
+        rollDice();
+        displayDice();
+        
+        const attackerIndex = gameData.index
+        const defenderIndex = 1- gameData.index;
+
+        // SNAKE EYES
+        if(gameData.rollSum === 2) {
+
+            game.innerHTML+= '<p>Oh snap! Snake eyes!</p>';
+        
+            gameData.score[gameData.index] = 0;
+            gameData.index = defenderIndex;
+
+            updateBar(attackerIndex);
+            showCurrentScore();
+            setTimeout(setUpTurn, 2000);
+            return;
+        }
+
+        // ONE 1
+        else if(gameData.roll1 === 1 || gameData.roll2 === 1) {
+            game.innerHTML += `<p>Sorry, one of your rolls was a one, switching to ${gameData.players[defenderIndex]}</p>`;
+
+            switchPlayer();
+            setTimeout(setUpTurn, 2000);
+            return;
+        }
+
+        // ATTACK
+        else if(gameData.roll1 === 6 || gameData.roll2 === 6) {
+            
+            game.innerHTML += '<p>A direct hit!</p>';
+
+            let damage = gameData.roll1 === 6 ? gameData.roll2 : gameData.roll1;
+
+            gameData.score[defenderIndex] = Math.max(0, gameData.score[defenderIndex] - damage); 
+    
+            updateBar(defenderIndex);
+            celebrate(attackerIndex);
+        }
+
+        // NORMAL ROLL
+        else {
+            gameData.score[attackerIndex] += gameData.rollSum;
+
+            updateBar(attackerIndex);
+        }
+
+        console.log(
+            `player 1 score: ${gameData.score[0]} 
+            player 2 score: ${gameData.score[1]}
+            ${gameData.roll1}, ${gameData.roll2}`
+        );
+     
+        proceed();
+        checkWinningCondition();
     }
 
 })();
